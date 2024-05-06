4412f495e695f5b40fd0f86fa556f8ec254a93fa
Eric-Obeng <obengeric.3958@gmail.com>
2024-05-06 18:41:22 +0000

    Day 6 completed

---
+++ Day-6_Python-Functions_and-Karel/main.py
@@ -0,0 +1,18 @@
+# code blocks
+# indentation
+# functoions
+# while loop
+
+
+# Functions
+def my_function():
+    print("Hello")
+    print("bye")
+
+# call or invoke function
+my_function()
+
+number_of_hurdles = 6
+while number_of_hurdles > 0:
+    print('Hi')
+    number_of_hurdles -= 1
